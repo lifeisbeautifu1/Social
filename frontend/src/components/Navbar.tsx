156db2ae4d5326c75fc33a321b666148776a7bcf
Potato <poltoradnevone@gmail.com>
2022-08-23 21:40:49 +0400

    small ui changes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -247,73 +247,74 @@ const Navbar: React.FC<NavbarProps> = ({ socket }) => {
               d="M19 9l-7 7-7-7"
             />
           </svg>
-          {showInfo && (
-            <div
-              ref={modal}
-              className="info flex flex-col w-[250%]  bg-white border border-gray-200 shadow rounded absolute top-[60px] right-[20%] md:right-[-50%] text-sm text-gray-500 font-semibold"
+
+          <div
+            ref={modal}
+            className={`info flex flex-col w-[250%]  bg-white border border-gray-200 shadow rounded absolute top-[55px] right-0 transition duration-150 text-sm text-gray-500 font-semibold origin-top-right ${
+              showInfo && 'active'
+            }`}
+          >
+            <Link
+              className="p-2 hover:bg-gray-100 cursor-pointer flex items-center gap-1"
+              to={`/profile/${user?._id}`}
             >
-              <Link
-                className="p-2 hover:bg-gray-100 cursor-pointer flex items-center gap-1"
-                to={`/profile/${user?._id}`}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4 text-[#5181b8]"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-[#5181b8]"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                  />
-                </svg>
-                Profile
-              </Link>
-              <Link
-                className="p-2 hover:bg-gray-100 cursor-pointer flex gap-1 items-center"
-                to="/"
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+              Profile
+            </Link>
+            <Link
+              className="p-2 hover:bg-gray-100 cursor-pointer flex gap-1 items-center"
+              to="/"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4 text-[#5181b8]"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-[#5181b8]"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
-                  />
-                </svg>
-                News
-              </Link>
-              <span
-                className="p-2 hover:bg-gray-100 cursor-pointer flex gap-1  hover:underline items-center"
-                onClick={() => dispatch(logout())}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+                />
+              </svg>
+              News
+            </Link>
+            <span
+              className="p-2 hover:bg-gray-100 cursor-pointer flex gap-1  hover:underline items-center"
+              onClick={() => dispatch(logout())}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4 text-[#5181b8]"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-[#5181b8]"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                  />
-                </svg>
-                Logout
-              </span>
-            </div>
-          )}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+              Logout
+            </span>
+          </div>
         </div>
       </div>
     </div>
